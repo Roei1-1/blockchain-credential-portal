57368a4bb83538f5d5b497cbb277e4fea06d3cf3
Roei1-1 <quianecleo@gmail.com>
2026-02-10 23:33:43 +0800

    Create App.js

---
+++ contracts/contracts/contracts/scripts/contracts/backend/backend/frontend/frontend/src/frontend/src/frontend/src/App.js
@@ -0,0 +1,12 @@
+import CredentialPortal from './CredentialPortal';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CredentialPortal />
+    </div>
+  );
+}
+
+export default App;
